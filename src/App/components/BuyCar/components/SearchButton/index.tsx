f67227d7a769815996e92298128f37e8e78766e7
iephyp <iephyp@gmail.com>
2018-12-17 17:20:27 +0100

    realized button logic

--- src/App/components/BuyCar/components/SearchButton/index.tsx
+++ src/App/components/BuyCar/components/SearchButton/index.tsx
@@ -2,15 +2,60 @@ import React, { Component } from 'react';
 import './style.scss';
 import { connect } from 'react-redux';
 import { Button } from '@material-ui/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
+import { State } from 'src/store/state';
 
-class SearchButton extends Component<any, any> {
+interface SearchButtonProps {
+  selectedBrandId: number;
+  selectedModelId: number;
+}
+
+class SearchButton extends Component<SearchButtonProps, any> {
+  private clickSubject = new Subject();
+  private unmounted = new Subject();
+
+  componentWillMount() {
+    this.clickSubject
+      .asObservable()
+      .pipe(
+        takeUntil(this.unmounted),
+        debounceTime(500)
+      )
+      .subscribe(() => alert('search started'));
+  }
+
+  handleClick = () => {
+    this.clickSubject.next();
+  }
+
   render() {
+    const { selectedBrandId, selectedModelId } = this.props;
+
     return (
       <div className="search-button">
-        <Button fullWidth={ true } variant="contained">Search</Button>
+        <Button
+          fullWidth={ true }
+          variant="contained"
+          onClick={ this.handleClick }
+          disabled={ selectedBrandId === -1 || selectedModelId === -1 }
+        >
+          Search
+        </Button>
       </div>
     );
   }
+
+  componentWillUnmount() {
+    this.clickSubject.complete();
+    this.unmounted.next();
+    this.unmounted.complete();
+  }
 }
 
-export default connect()(SearchButton);
+export default connect(
+  ({ selectedBrandId, selectedModelId }: State) => ({
+    selectedBrandId,
+    selectedModelId
+  })
+)(SearchButton);
