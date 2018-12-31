67873a3fbf2775dcdd20be45d000f5b2e00f60d5
iephyp <iephyp@gmail.com>
2018-12-31 18:00:49 +0100

    removed TextFilterItem component cuz it's used only once

--- src/App/components/common/TextFilterItem/test.utils.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import {
-  TextFilterItem, TextFilterItemProps
-} from '.';
-import { getConfiguredRender } from 'test-utils/configured-render';
-
-export const render = getConfiguredRender<TextFilterItemProps>(
-  <TextFilterItem />
-);
